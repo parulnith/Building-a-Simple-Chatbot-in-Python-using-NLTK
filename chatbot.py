3b295c5be6918a62dbab53207b8160d7933cd607
Parul Pandey <parulnith@gmail.com>
2019-05-28 10:31:54 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,8 +15,8 @@ from nltk.stem import WordNetLemmatizer
 nltk.download('popular', quiet=True) # for downloading packages
 
 # uncomment the following only the first time
-#nltk.download('punkt') # first-time use only
-#nltk.download('wordnet') # first-time use only
+nltk.download('punkt') # first-time use only
+nltk.download('wordnet') # first-time use only
 
 
 #Reading in the corpus
